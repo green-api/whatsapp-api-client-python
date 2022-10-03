fe552ea9e93473a1baac9a8c6be4dc3072d3a2ee
sadiv <sadiv@bk.ru>
2022-10-03 21:59:01 +0300

    Test

--- test_green.py
+++ test_green.py
@@ -1,11 +1,11 @@
-import os
+from os import environ
 from re import T
 from green_api.account import *
 from green_api.main import GreenApi
 
 
-ID_INSTANCE = os.environ['ID_INSTANCE']
-API_TOKEN_INSTANCE = os.environ['API_TOKEN_INSTANCE']
+ID_INSTANCE = environ['ID_INSTANCE']
+API_TOKEN_INSTANCE = environ['API_TOKEN_INSTANCE']
 
 g = GreenApi('https://api.green-api.com', 
                         ID_INSTANCE, 
