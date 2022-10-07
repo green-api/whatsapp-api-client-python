b2316a063b943bb850ee69f92cf0310ddfe8c63c
sadiv <sadiv@bk.ru>
2022-10-07 11:59:54 +0300

    packages

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description=read_me_description,
     long_description_content_type="text/markdown",
     url="https://github.com/green-api/whatsapp-api-client-python",
-    packages=['whatsapp_api_client_python'],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
