24801f2c3ef6786a131d61722a71dc97a49a22b6
sadiv <sadiv@bk.ru>
2022-10-07 09:20:29 +0300

    Bugfix

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,15 +6,15 @@ from whatsapp_api_client_python import API as API
 ID_INSTANCE = environ['ID_INSTANCE']
 API_TOKEN_INSTANCE = environ['API_TOKEN_INSTANCE']
 
-API = API.RestApi('https://api.green-api.com', 
+RestApi = API.RestApi('https://api.green-api.com', 
                         ID_INSTANCE, 
                         API_TOKEN_INSTANCE)
 
 class TestClass: 
         def test_getSettings(self):            
-                result = API.account.getSettings()
+                result = RestApi.account.getSettings()
                 assert isinstance(result, API.Response) and result.code == 200
 
         def test_getStateInstance(self):            
-                result = API.account.getStateInstance()
+                result = RestApi.account.getStateInstance()
                 assert isinstance(result, API.Response) and result.code == 200
