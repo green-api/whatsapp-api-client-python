c8da6ef36b21a9e4ff5f41aff979f2c534cb99fb
sadiv <sadiv@bk.ru>
2022-10-10 11:35:24 +0300

    After audit

--- examples/createGroupAndSendMessage.py
+++ examples/createGroupAndSendMessage.py
@@ -1,6 +1,6 @@
 from os import environ
 
-from whatsapp_api_client_python import API as API
+from whatsapp_api_client_python import greenAPI as greenAPI
 
 ID_INSTANCE = environ['ID_INSTANCE']
 API_TOKEN_INSTANCE = environ['API_TOKEN_INSTANCE']
