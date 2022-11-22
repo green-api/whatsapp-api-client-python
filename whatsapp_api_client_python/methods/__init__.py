379270993ec43de6b088f6d30bb4e345c55201c6
Amele9 <amele090044@gmail.com>
2022-11-22 21:06:35 +0500

    `DeviceCategory` class updates

--- whatsapp_api_client_python/methods/__init__.py
+++ whatsapp_api_client_python/methods/__init__.py
@@ -1,3 +1,4 @@
 from whatsapp_api_client_python.methods.account import AccountCategory
+from whatsapp_api_client_python.methods.device import DeviceCategory
 
-__all__ = ["AccountCategory"]
+__all__ = ["AccountCategory", "DeviceCategory"]
