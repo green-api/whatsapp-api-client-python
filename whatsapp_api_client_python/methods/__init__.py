3464d3554ae7d54c4ee666693532d60e3fd88c08
Amele9 <amele090044@gmail.com>
2022-11-22 21:55:53 +0500

    `QueueCategory` class updates

--- whatsapp_api_client_python/methods/__init__.py
+++ whatsapp_api_client_python/methods/__init__.py
@@ -1,5 +1,11 @@
 from whatsapp_api_client_python.methods.account import AccountCategory
 from whatsapp_api_client_python.methods.device import DeviceCategory
 from whatsapp_api_client_python.methods.mark_read import MarkReadCategory
+from whatsapp_api_client_python.methods.queues import QueueCategory
 
-__all__ = ["AccountCategory", "DeviceCategory", "MarkReadCategory"]
+__all__ = [
+    "AccountCategory",
+    "DeviceCategory",
+    "MarkReadCategory",
+    "QueueCategory"
+]
