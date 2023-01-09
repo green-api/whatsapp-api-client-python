68c2cdaa98bb782dd211876faf0d7dfedd4235a5
Amele9 <amele090044@gmail.com>
2023-01-09 17:13:42 +0500

    Some updates

--- examples/createGroupAndSendMessage.py
+++ examples/createGroupAndSendMessage.py
@@ -1,6 +1,9 @@
 from whatsapp_api_client_python import GreenAPI
 
-greenAPI = GreenAPI("ID_INSTANCE", "API_TOKEN_INSTANCE")
+ID_INSTANCE = "1101000001"
+API_TOKEN_INSTANCE = "3e03ea9ff3324e228ae3dfdf4d48e409bfa1b1ad0b0c46bf8c"
+
+greenAPI = GreenAPI(ID_INSTANCE, API_TOKEN_INSTANCE)
 
 
 def main():
