d996ad29dbe86b17793c71e47b0690630fb59fde
ripreal <ripreal1@yandex.ru>
2023-01-30 17:27:39 +0300

    update to 0.0.31

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as file:
 
 setuptools.setup(
     name="whatsapp-api-client-python",
-    version="0.0.27",
+    version="0.0.31",
     install_requires=['requests'],
     author="Ivan Sadovy",
     author_email="sadiv@bk.ru",
