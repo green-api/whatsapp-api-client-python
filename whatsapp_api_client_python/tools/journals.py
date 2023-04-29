ff81670af1672332cbb7b51662dd3f89108b2cfa
Amele9 <amele090044@gmail.com>
2023-04-29 13:58:11 +0500

    Added method `GetMessage`

--- whatsapp_api_client_python/tools/journals.py
+++ whatsapp_api_client_python/tools/journals.py
@@ -1,33 +1,77 @@
-from whatsapp_api_client_python.response import Response
+from typing import Optional, TYPE_CHECKING
+
+from ..response import Response
+
+if TYPE_CHECKING:
+    from ..API import GreenApi
+
 
 class Journals:
-    def __init__(self, greenApi) -> None:
-        self.greenApi = greenApi
-        
-    def getChatHistory(self, chatId: str, count: str) -> Response:
-            'The method returns the chat message history.'
+    def __init__(self, api: GreenApi):
+        self.api = api
 
-            requestBody = {
-                'chatId': chatId,
-                'count': count,
-            }
+    def get_chat_history(
+            self, chat_id: str, count: Optional[int] = None
+    ) -> Response:
+        """The method returns the chat message history."""
 
-            return self.greenApi.request('POST', 
-                '{{host}}/waInstance{{idInstance}}'
-                '/GetChatHistory/{{apiTokenInstance}}',
-                requestBody)
+        request_body = locals()
+        if count is None:
+            del request_body["count"]
+        del request_body["self"]
 
-    def lastIncomingMessages(self) -> Response:
-            'The method returns the chat message history.'
+        return self.api.request(
+            "POST", (
+                "{{host}}/waInstance{{idInstance}}/"
+                "GetChatHistory/{{apiTokenInstance}}"
+            ), request_body
+        )
 
-            return self.greenApi.request('GET', 
-                '{{host}}/waInstance{{idInstance}}'
-                '/LastIncomingMessages/{{apiTokenInstance}}')
+    def get_message(self, chat_id: str, id_message: str) -> Response:
+        """The method returns the chat message."""
 
-    def lastOutgoingMessages(self) -> Response:
-            'The method returns the last outgoing messages of the account.'
-            'Outgoing messages are stored on the server for 24 hours.'
+        request_body = locals()
+        del request_body["self"]
 
-            return self.greenApi.request('GET', 
-                '{{host}}/waInstance{{idInstance}}'
-                '/LastOutgoingMessages/{{apiTokenInstance}}')
+        return self.api.request(
+            "POST", (
+                "{{host}}/waInstance{{idInstance}}/"
+                "getMessage/{{apiTokenInstance}}"
+            ), request_body
+        )
+
+    def last_incoming_messages(
+            self, minutes: Optional[int] = None
+    ) -> Response:
+        """
+        The method returns the last incoming messages of the account.
+        """
+
+        request_body = None
+        if minutes is not None:
+            request_body = {"minutes": minutes}
+
+        return self.api.request(
+            "GET", (
+                "{{host}}/waInstance{{idInstance}}/"
+                "lastIncomingMessages/{{apiTokenInstance}}"
+            ), request_body
+        )
+
+    def last_outgoing_messages(
+            self, minutes: Optional[int] = None
+    ) -> Response:
+        """
+        The method returns the last outgoing messages of the account.
+        """
+
+        request_body = None
+        if minutes is not None:
+            request_body = {"minutes": minutes}
+
+        return self.api.request(
+            "GET", (
+                "{{host}}/waInstance{{idInstance}}/"
+                "lastIncomingMessages/{{apiTokenInstance}}"
+            ), request_body
+        )
