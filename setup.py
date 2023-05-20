955e75dfd662eced15e7f081a6665cbbfbe52760
Amele9 <amele090044@gmail.com>
2023-05-20 15:24:43 +0500

    Updated `setup.py`

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md") as file:
-    read_me_description = file.read()
+    long_description = file.read()
 
-setuptools.setup(
+setup(
     name="whatsapp-api-client-python",
-    version="0.0.34",
-    install_requires=['requests'],
-    author="Ivan Sadovy",
-    author_email="sadiv@bk.ru",
-    description="This library helps you easily create a python '\
-        'application to connect the WhatsApp API using service green-api.com",
-    long_description=read_me_description,
+    version="0.0.35",
+    description=(
+        "This library helps you easily create"
+        " a Python application with WhatsApp API."
+    ),
+    long_description=long_description,
     long_description_content_type="text/markdown",
+    author="GREEN API",
+    author_email="support@green-api.com",
     url="https://github.com/green-api/whatsapp-api-client-python",
-    packages=['whatsapp_api_client_python', 'whatsapp_api_client_python.tools'],
+    packages=find_packages(exclude=["examples", "tests"]),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Application Frameworks"
     ],
-    python_requires='>=3.5',
+    license="Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0)",
+    install_requires=["requests==2.30.0"],
+    python_requires=">=3.7"
 )
