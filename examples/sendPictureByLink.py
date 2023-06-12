476eb940caec22824bfab21d0c0959f1e1918c0a
Amele9 <amele090044@gmail.com>
2023-06-12 16:40:49 +0500

    Updated examples

--- examples/sendPictureByLink.py
+++ examples/sendPictureByLink.py
@@ -1,15 +1,19 @@
-from whatsapp_api_client_python import API as API
+from whatsapp_api_client_python import API
 
-ID_INSTANCE = '1101000001'
-API_TOKEN_INSTANCE = '3e03ea9ff3324e228ae3dfdf4d48e409bfa1b1ad0b0c46bf8c'
+greenAPI = API.GreenApi(
+    "1101000001", "d75b3a66374942c5b3c019c698abc2067e151558acbd412345"
+)
 
-greenAPI = API.GreenApi(ID_INSTANCE, API_TOKEN_INSTANCE)
 
 def main():
-    result = greenAPI.sending.sendFileByUrl('11001234567@c.us', 
-        'https://www.google.ru/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png', 
-        'googlelogo_color_272x92dp.png', 'Google logo')
-    print(result.data)
+    response = greenAPI.sending.sendFileByUrl(
+        "11001234567@c.us",
+        "https://green-api.com/green-api-logo_2.png",
+        "green-api-logo_2.png"
+    )
 
-if __name__ == "__main__":
+    print(response.data)
+
+
+if __name__ == '__main__':
     main()
--- examples/sendTextMessage.py
+++ examples/sendTextMessage.py
@@ -1,13 +1,15 @@
-from whatsapp_api_client_python import API as API
+from whatsapp_api_client_python import API
 
-ID_INSTANCE = '1101000001'
-API_TOKEN_INSTANCE = '3e03ea9ff3324e228ae3dfdf4d48e409bfa1b1ad0b0c46bf8c'
+greenAPI = API.GreenApi(
+    "1101000001", "d75b3a66374942c5b3c019c698abc2067e151558acbd412345"
+)
 
-greenAPI = API.GreenApi(ID_INSTANCE, API_TOKEN_INSTANCE)
 
 def main():
-    result = greenAPI.sending.sendMessage('11001234567@c.us', 'Message text')
-    print(result.data)
+    response = greenAPI.sending.sendMessage("11001234567@c.us", "Message text")
 
-if __name__ == "__main__":
+    print(response.data)
+
+
+if __name__ == '__main__':
     main()
