8e2d8c399ad2e15df91a960f6bf6ce6ef1bc4ac1
Amele9 <amele090044@gmail.com>
2023-07-01 15:37:05 +0500

    Some refactoring

--- whatsapp_api_client_python/response.py
+++ whatsapp_api_client_python/response.py
@@ -1,16 +1,15 @@
-import json
+from json import loads
+from typing import Optional
 
 
 class Response:
-    code: int
-    data: json
-    error: str
+    code: Optional[int]
+    data: Optional[dict] = None
+    error: Optional[str] = None
 
-    def __init__(self, code: int, text: str) -> None:
+    def __init__(self, code: Optional[int], text: str):
         self.code = code
-        if code == 200:
-            self.data = json.loads(text)
-            self.error = None
+        if self.code == 200:
+            self.data = loads(text)
         else:
             self.error = text
-            self.data = None
