b94f3ebf3732b7b9e255d172ebc92b45cacfeab7
Amele9 <amele090044@gmail.com>
2023-11-28 05:48:07 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="UTF-8") as file:
 
 setup(
     name="whatsapp-api-client-python",
-    version="0.0.43",
+    version="0.0.44",
     description=(
         "This library helps you easily create"
         " a Python application with WhatsApp API."
