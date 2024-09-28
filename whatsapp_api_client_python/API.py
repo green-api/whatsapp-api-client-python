74f25e3e916aebf6a70e828ec12d437cef1578e3
xTimop <112824210+xTimop@users.noreply.github.com>
2024-09-28 16:00:04 +0300

    add timeout to request

--- whatsapp_api_client_python/API.py
+++ whatsapp_api_client_python/API.py
@@ -75,11 +75,11 @@ class GreenApi:
         try:
             if not files:
                 response = self.session.request(
-                    method=method, url=url, json=payload
+                    method=method, url=url, json=payload, timeout=30
                 )
             else:
                 response = self.session.request(
-                    method=method, url=url, data=payload, files=files
+                    method=method, url=url, data=payload, files=files, timeout=30
                 )
         except Exception as error:
             error_message = f"Request was failed with error: {error}."
