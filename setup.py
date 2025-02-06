fb76f34ff98973c7223995b240323c8d0578a30e
prostraction <47314760+prostraction@users.noreply.github.com>
2025-02-06 17:42:11 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="UTF-8") as file:
 
 setup(
     name="whatsapp-api-client-python",
-    version="0.0.48",
+    version="0.0.49",
     description=(
         "This library helps you easily create"
         " a Python application with WhatsApp API."
